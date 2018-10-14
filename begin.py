c6d9d878fdeea25ff55325c0437fd7225e158d7b
Hansen06 <haishenmuc@163.com>
2018-10-14 11:37:16 +0800

    first commint

---
+++ begin.py
@@ -0,0 +1,8 @@
+from scrapy import cmdline
+
+
+# cmdline.execute("scrapy crawl tibet_people".split())
+# cmdline.execute("scrapy crawl tibet_tibet3".split())
+# cmdline.execute("scrapy crawl tb_tibet".split())
+# cmdline.execute("scrapy crawl tibet_xinhua".split())
+cmdline.execute("scrapy crawl tibetcm".split())
